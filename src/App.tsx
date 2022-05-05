a80702a3bc72b5888e929929349077a0d807e6fd
ThallesP <contato@thalles.me>
2022-05-05 16:50:11 -0300

    :sparkles: class 04

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,21 @@ import { ReactComponent as FacebookLogo } from "../assets/facebook.svg";
 import { ReactComponent as YoutubeLogo } from "../assets/youtube.svg";
 import { ReactComponent as CloseSVG } from "../assets/close.svg";
 import { ReactComponent as CheckmarkSVG } from "../assets/checkmark.svg";
-import { ReactComponent as DoctorSVG } from "../assets/doctor.svg";
+import { ReactComponent as MapPinSVG } from "../assets/mapPin.svg";
+import { ReactComponent as MailSVG } from "../assets/mail.svg";
+import { ReactComponent as UpIconSVG } from "../assets/upIcon.svg";
 import ScrollReveal from "scrollreveal";
 
 import "./App.css";
-import React, { SyntheticEvent, useRef } from "react";
+import React, { SyntheticEvent } from "react";
 
 function App() {
-  function onScrollNav(ev: Event): void {
-    const scrollTop = (ev.target as HTMLDivElement).scrollTop;
+  function onScroll(ev: Event) {
+    showGreenNavOnScroll();
+    showBackToTopButton();
+  }
+
+  function showGreenNavOnScroll(): void {
     const nav = document.getElementById("navbar");
     if (scrollY == 0) {
       nav?.classList.remove("scroll");
@@ -25,6 +31,16 @@ function App() {
     }
 
     nav?.classList.add("scroll");
+  }
+
+  function showBackToTopButton(): void {
+    const button = document.getElementById("backToTopButton");
+    if (scrollY < 500) {
+      button?.classList.remove("show");
+      return;
+    }
+
+    button?.classList.add("show");
   }
 
   function openMenu(e: SyntheticEvent): void {
@@ -44,7 +60,7 @@ function App() {
     element?.scrollIntoView({ behavior: "smooth" });
   }
 
-  document.addEventListener("scroll", onScrollNav);
+  document.addEventListener("scroll", onScroll);
 
   ScrollReveal({ origin: "top", distance: "30px", duration: 700 }).reveal(
     `#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content`
@@ -95,7 +111,15 @@ function App() {
             </ul>
 
             <button className="button">
-              <a href="#">Agende sua consulta</a>
+              <a
+                onClick={(e) => {
+                  closeMenu(e);
+                  scrollTo("contact");
+                }}
+                href="#contact"
+              >
+                Agende sua consulta
+              </a>
             </button>
 
             <ul className="social-links">
@@ -150,9 +174,9 @@ function App() {
               Os médicos da DoctorCare vão além dos sintomas para tratar a causa
               raiz de sua doença e proporcionar uma cura a longo prazo.
             </p>
-            <button>
+            <a className="button" href="#contact">
               <WhatsappLogo /> Agende sua consulta
-            </button>
+            </a>
 
             <GirlImg
               id="girl-svg"
@@ -277,6 +301,75 @@ function App() {
           </div>
         </div>
       </section>
+
+      <section id="contact">
+        <div className="wrapper">
+          <header>
+            <h2>Entre em contato com a gente!</h2>
+          </header>
+        </div>
+
+        <div className="content">
+          <div className="wrapper">
+            <ul>
+              <li>
+                <MapPinSVG /> R. Amauri Souza, 346
+              </li>
+              <li>
+                <MailSVG />
+                contato@doctorcare.com
+              </li>
+            </ul>
+            <a
+              className="button"
+              target="_blank"
+              href="https://wa.me/5511999999999"
+            >
+              <WhatsappLogo /> Agende sua consulta
+            </a>
+            <img
+              src="/assets/black-guy-cellphone.png"
+              alt="homem negro com moletom bege mexendo no celular e sorrindo"
+            />
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div className="wrapper">
+          <a className="logo" href="#">
+            <Logo />
+          </a>
+          <p>
+            ©2022 DoctorCare.
+            <br />
+            Todos os direitos reservados.
+          </p>
+          <ul className="social-links">
+            <li>
+              <a target="_blank" href="https://facebook.com">
+                <FacebookLogo />
+              </a>
+            </li>
+
+            <li>
+              <a target="_blank" href="https://instagram.com">
+                <InstagramLogo />
+              </a>
+            </li>
+
+            <li>
+              <a target="_blank" href="https://youtube.com">
+                <YoutubeLogo />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+
+      <a id="backToTopButton" href="#home">
+        <UpIconSVG />
+      </a>
     </div>
   );
 }
